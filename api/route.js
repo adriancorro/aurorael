47f82661adda9c605b340d9a5a327485633f96c1
adriancorro <adriancorrog@gmail.com>
2026-02-20 17:30:37 +0100

    new version text mobile

--- api/route.js
+++ api/route.js
@@ -108,7 +108,7 @@ export async function POST(req) {
     const askAuthor = KEYWORDS.some((k) => clean.includes(normalizeText(k)));
     if (askAuthor) {
       const txt =
-        "Hola, estoy listo para hablar sobre cualquier idea ¿En qué piensas? Mientras piensas en algo, quiero decir que soy Aurorael un modelo de lenguaje avanzado filosófico-crítico y si teienes interés te muestro un video donde se muestra como se desarrolló Aurorael. El gran inicio";
+        "Hola, estoy listo para hablar sobre cualquier idea ¿En qué piensas? Mientras piensas en algo, quiero decir que soy Aurorael un modelo de lenguaje avanzado filosófico-crítico y si tienes interés te muestro un video donde se muestra como se desarrolló Aurorael. El gran inicio";
 
       return new Response(
         JSON.stringify({ result: txt, videoId: "jOSO3AAIUzM", sessionId: id }),
